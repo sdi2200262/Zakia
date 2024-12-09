c9669c5e6d7f77e2c1b3266f6c2407591a913275
Ripymmt <t.nick.sp@gmail.com>
2024-12-09 17:38:48 +0200

    datanode.c

--- YSBD-MAN-FOUDAS/Exercise2/bplus/src/bp_datanode.c
+++ YSBD-MAN-FOUDAS/Exercise2/bplus/src/bp_datanode.c
@@ -8,6 +8,14 @@
 
 #define MAX_RECS 2  // opos sto paradeigma sto power point
 
+
+/* mesa sto file auto periexontai sinarthseis pou diaxeirizontai tous kombous fylla enos bplus tree, oi  opoioi komvoi einai 
+    sthn ousia komvoi dedomenon(opos anaferetai k sth theoria). Oi sinartiseis autes aforoun basikes leitoyrgies pano se komvous
+    opos arxikopoihsh, eisagwgh , anazhthsh alla kai diaxwrismos komvou ean se auton yparxoun parapanw apo MAX_RECORDS eggrafes.
+    Kathe komvos mporei na anaparastathei ws mia domh DataNode h opoia mesa periexei simantikes plirofories opos plithos 
+    eggrafwn(counter),enan deikth se epomeno block kai enan pinaka me tis eggrafes pou briskontai mesa sto current block(node), 
+    o opoios exei statiko megethos kai iso me MAX_RECORDS */
+
 typedef struct DataNode
 {
     int nextBlock;  // pointer
@@ -16,7 +24,16 @@ typedef struct DataNode
 }DataNode;
 
 
-int insert(BF_Block *block, Record *rec, bool split){
+int initializeDataNode(DataNode* node){
+    if(node !=  NULL){
+        node->recCount = 0;
+        node->nextBlock = -1;
+        return 0;
+    }else return -1;
+}
+
+
+int insertToDataNode(BF_Block *block, Record *rec, bool split){
     char* data = BF_Block_GetData(block);
     // dereference to data apo type Block se type DataNode
     DataNode* node = (DataNode *) data;
@@ -29,9 +46,10 @@ int insert(BF_Block *block, Record *rec, bool split){
     }
 
     // den yparxei opote prepei na vrei thn swsth thesh gia na bei h eggrafh
-    int j = node->recCount -1;
+    int j = node->recCount - 1 ;
     while (j>=0 && node->recs[j].id > rec->id){
-        node->recs[j+1] = node->recs[j];
+        node->recs[j+1] = node->recs[j];    //metakinh oles tis eggrafes mia thesh pisw gia na yparjei xwros
+                                            //gia thn eggrafh pou theloume na kanoume insert
         j--;
     }
     node->recs[j+1] = *rec; // efoson to epitrepomeno orio records ana node den paraviazete
@@ -52,16 +70,25 @@ int split_data(BF_Block *block, BF_Block *newblock, Record *rec){
     char* new_data = BF_Block_GetData(newblock);
     DataNode* new_node = (DataNode*) new_data;
 
-    int mid = node->recCount / 2;
-    new_node->recCount = node->recCount - mid;  // to neo node exei arxika xwro gia tis mises eggrafes tou paliou node
-    memcpy(new_node->recs , &node->recs[mid], new_node->recCount * sizeof(Record));
+    if (node->recCount > MAX_RECS){
+        // arxikopoioume to neo komvo
+        initializeDataNode(new_node);
+        int splitIndex = node->recCount / 2;
+        // ta misa recs tou current node bainoun sto new node
+        for(int i = 0; i < splitIndex; i++) {
+            insertToDataNode(new_node, &node->recs[i], false);
+        }
 
-    new_node->nextBlock = node->nextBlock;  // to neo node tha deixnei sto block pou edeixne to prohgoumeno node 
+        node->recCount = splitIndex;
+        // to pointer se epomeno block tou neo komvou einai auto tou current node
 
-    node->recCount = mid;
-    *rec = new_node->recs[0]; //h nea eggrafh pou dinete san orisma tha bei sth prwth thesh tou neou node
-
-    return 0;
+        new_node->nextBlock = node->nextBlock;
+        node->nextBlock = -1;
+        return 0;
+    }else{
+        printf("No splitting needed...\n");
+        return -1;
+    }
 }
 
 int search_record(BF_Block *block, int id, Record **result){
@@ -71,15 +98,12 @@ int search_record(BF_Block *block, int id, Record **result){
                                         // gia kathe eggrafh kai na diaxeirizetai tis plirofories ths gia to current node
 
     for(int i = 0; i < node->recCount; i++){
-        if(node->recs[i].id == id){ // an brethei h eggrafh pou psaxnoume me record->id == id
-            *result = &node->recs[i];   // pernate h diefthinsh ths ston deikth result kai aytos me th seira tou
-                                        // tha xrhshmopoieithei sthn sunarthsh BPLUS_GET_ENTRY opou ths epistrafei
-                                        // to periexomeno ths egrafhs auths 
-            return 0;
+        if(node->recs[i].id == id){      // an brethei h eggrafh pou psaxnoume me record->id == id
+            *result = &node->recs[i];   // pernate h diefthinsh ths ston deikth result
+            return i; // epistrefete h thesh sthn opoia brethike h eggrafh
         }
     }
 
-    *result = NULL; // an de vrethei h eggrafh tote to result pernei thn timh null kai
-                    // h sunarthsh GET_ENTRY epistrefei null
+    *result = NULL; // an de vrethei h eggrafh tote to result pernei thn timh null
     return -1;
 }
